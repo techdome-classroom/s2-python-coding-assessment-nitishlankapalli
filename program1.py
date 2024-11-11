c926f7377c11b85256bdb23c8ca9646a75581159
nitish <lankalapallinitish@gmail.com>
2024-11-11 15:12:12 +0530

    submitting python code

--- program1.py
+++ program1.py
@@ -4,7 +4,22 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+
+        matching_brackets = {')': '(', '}': '{', ']': '['}
+        stack = []
+
+        for char in s:
+            if char in matching_brackets:
+
+                top_element = stack.pop() if stack else '#'
+                if matching_brackets[char] != top_element:
+                    return False
+            else:
+
+                stack.append(char)
+
+
+        return not stack
 
 
 
--- program2.py
+++ program2.py
@@ -4,7 +4,32 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
 
+        roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+
+            current_val = roman_values[s[i]]
+            
+
+            if i < n - 1 and current_val < roman_values[s[i + 1]]:
+
+                total -= current_val
+            else:
+
+                total += current_val
+                
+        return total
 
 
